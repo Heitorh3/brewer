fb3c501d768640b529ea4f0ef17c5f5d5026312e
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-03-22 19:24:38 -0300

    Configurando o Layout dialect

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.brewer.config;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -44,6 +46,8 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
+		
+		engine.addDialect(new LayoutDialect());
 		return engine;
 	}
 	
