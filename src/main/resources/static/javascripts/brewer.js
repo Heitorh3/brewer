91c7f187139242ab7a81a54b4e467778db1798ff
Heitor Neto <heitorh3@gmail.com>
2017-03-23 09:53:09 -0300

    Formatando a formatação com jquery maskMoney

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,31 @@
+var Brewer = Brewer || {};
+
+Brewer.Formatar = (function(){
+	
+	function Formatar(){
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+	
+	Formatar.prototype.iniciar = function(){
+		this.decimal.maskMoney();
+		
+		this.plain.maskMoney({ precision: 0 });
+	}
+
+	return Formatar;
+})();
+
 $(function() {
+	var decimal = new Brewer.Formatar();
+	decimal.iniciar();
+});
+
+
+/*$(function() {
 	var decimal = $('.js-decimal');
 	decimal.maskMoney();
 	
 	var plain = $('.js-plain');
 	plain.maskMoney({ precision: 0 });
-});
+});*/
