5302d9d31ea9e036f68972b5f07722f927396fee
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-03-23 21:45:53 -0300

    prototipando o cadastro de cidade

---
+++ src/main/java/com/algaworks/brewer/controller/CidadeController.java
@@ -0,0 +1,30 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Cidade;
+
+@Controller
+public class CidadeController {
+
+	@RequestMapping("/cidades/novo")
+	public String novo(Cidade cidade){
+		return "cidade/CadastrarCidade";
+	}
+	
+	public String salvar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes){
+		if(result.hasErrors()){
+			return novo(cidade);
+		}
+		
+		attributes.addFlashAttribute("mensagem","Cidade cadastrada com sucesso");
+		
+		return "redirect:cidades/novo";
+	}
+	
+}
