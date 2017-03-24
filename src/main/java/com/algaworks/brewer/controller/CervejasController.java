148bad5defe4a21ac59061e2a7383c9942a0072b
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-03-24 14:04:03 -0300

    Montando os combos e radio do cadastro da cerveja

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,44 +2,46 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
-import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
 
-	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+	//private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 	
 	@Autowired
-	private Cervejas cervejas;
+	private Estilos estilos;
 	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja){
-		
-		cervejas.findAll();
-		
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja){
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()){
 			model.addAttribute(cerveja);
 			return novo(cerveja);
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cerveja cadastrada com sucesso.");
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 }
