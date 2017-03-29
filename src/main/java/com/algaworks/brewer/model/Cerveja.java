1d472199593b807181823ce04d53ba13fec14ebf
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-03-29 19:08:57 -0300

    Salvando o nome da foto da cerveja no banco de dados.

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -81,6 +81,11 @@ public class Cerveja implements Serializable {
 	@JoinColumn(name = "codigo_estilo")
 	private Estilo estilo;
 	
+	private String foto;
+	
+	@Column(name = "content_type")
+	private String contentType;
+	
 	@PrePersist
 	@PreUpdate
 	private void prePersisteUpdate(){
@@ -173,6 +178,22 @@ public class Cerveja implements Serializable {
 
 	public void setEstilo(Estilo estilo) {
 		this.estilo = estilo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
 	}
 
 	@Override
--- src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -3,13 +3,15 @@ package com.algaworks.brewer.storage;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.brewer.dto.FotoDTO;
+
 public class FotoStorageRunnable implements Runnable {
 
 	private MultipartFile[]files;
-	private DeferredResult<String> resultado;
+	private DeferredResult<FotoDTO> resultado;
 	
 
-	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<String> resultado) {
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado) {
 		super();
 		this.files = files;
 		this.resultado = resultado;
@@ -18,10 +20,13 @@ public class FotoStorageRunnable implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println(">>> files " + files[0].getName());
 		
 		//TODO: salvar a foto no sistema de arquivo
-		resultado.setResult("Foto recebida");
+		
+		String nome = files[0].getOriginalFilename();
+		String contentType = files[0].getContentType();
+		
+		resultado.setResult(new FotoDTO(nome, contentType));
 	}
 
 }
