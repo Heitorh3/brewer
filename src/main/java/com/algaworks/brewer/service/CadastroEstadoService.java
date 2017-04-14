024fac608051c3cd4c76ea711e4dfb2e0bb5bc4f
Heitor Neto <heitorh3@gmail.com>
2017-04-14 15:10:46 -0300

    Correção dos imports.

--- src/main/java/com/algaworks/brewer/service/CadastroEstadoService.java
+++ src/main/java/com/algaworks/brewer/service/CadastroEstadoService.java
@@ -1,10 +1,14 @@
 package com.algaworks.brewer.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.brewer.model.Estado;
 import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.service.exception.NomeEstadoJaCadastradoException;
 
 @Service
 public class CadastroEstadoService {
@@ -12,7 +16,13 @@ public class CadastroEstadoService {
 	@Autowired
 	private Estados estados;
 	
+	@Transactional
 	public void salvar(Estado estado){
+		Optional<Estado>estadoOptional = estados.findByNome(estado.getNome());
+		
+		if(estadoOptional.isPresent()){
+			throw new NomeEstadoJaCadastradoException("Nome estado já cadastrado");
+		}
 		estados.save(estado);
 	}
 	
