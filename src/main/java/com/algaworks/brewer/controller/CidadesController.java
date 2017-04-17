a8d92332cbe4ff22ee1d7d25990e49079bc09878
Heitor Neto <heitorh3@gmail.com>
2017-04-17 13:47:53 -0300

    Selecionado cidades pelo estado com Ajax

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -1,18 +1,29 @@
 package com.algaworks.brewer.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.Cidades;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadesController {
 
-	@RequestMapping("/cidades/novo")
+	@Autowired
+	private Cidades cidades;
+	
+	@RequestMapping("/novo")
 	public String novo(Cidade cidade){
 		return "cidade/CadastrarCidade";
 	}
@@ -25,6 +36,14 @@ public class CidadesController {
 		attributes.addFlashAttribute("mensagem","Cidade cadastrada com sucesso");
 		
 		return "redirect:cidades/novo";
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstao(@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado){
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {	}
+		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 	
 }
