6efa17ecab5d80cdb2d71a3602d56e41a3b6806d
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-04-21 10:01:29 -0300

    Adicionando os campos nome cidade e nome estadado.

--- src/main/java/com/algaworks/brewer/model/TipoPessoa.java
+++ src/main/java/com/algaworks/brewer/model/TipoPessoa.java
@@ -5,8 +5,18 @@ import com.algaworks.brewer.model.validation.group.CpfGroup;
 
 public enum TipoPessoa {
 
-	FISICA("Física", "CPF","000.000.000-00",CpfGroup.class),
-	JURIDICA ("Jurídica","CNPJ","00.000/0000-00",CnpjGgroup.class);
+	FISICA("Física", "CPF","000.000.000-00",CpfGroup.class) {
+		@Override
+		public String formatar(String cpfOuCnpj) {
+			return cpfOuCnpj.replaceAll("(\\d{3})(\\d{3})(\\d{3})","$1.$2.$3-");
+		}
+	},
+	JURIDICA ("Jurídica","CNPJ","00.000/0000-00",CnpjGgroup.class) {
+		@Override
+		public String formatar(String cpfOuCnpj) {
+			return cpfOuCnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})","$1.$2.$3/$4-");
+		}
+	};
 	
 	private String descricao;
 	private String documento;
@@ -40,4 +50,6 @@ public enum TipoPessoa {
 		return cpfOuCnpj.replaceAll("\\.|-|/", "");
 	}
 
+	public abstract String formatar(String cpfOuCnpj);
+		
 }
