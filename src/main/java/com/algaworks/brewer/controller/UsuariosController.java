8c1f998dc09ea5ea0ddb067212960f400a31f2fd
Heitor Neto <heitorh3@gmail.com>
2017-04-28 15:59:35 -0300

    Começando o cadastro do usuário validando os campos.

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -2,29 +2,41 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.service.CadastroUsuarioService;
 
 @Controller
+@RequestMapping("/usuarios")
 public class UsuariosController {
 
-	@RequestMapping("/usuarios/novo")
-	public String novo(Usuario usuario){
-		return "usuario/CadastroUsuario";
+	@Autowired
+	private CadastroUsuarioService usuarioService;
+	
+	@GetMapping("/novo")
+	public ModelAndView novo(Usuario usuario){
+		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
+		return mv;
 	}
 	
-	@RequestMapping("/usuarios/salvar")
-	public String salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes){
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes){
 		if(result.hasErrors()){
 			return novo(usuario);
 		}
 		
+		usuarioService.salvar(usuario);
+		
 		attributes.addFlashAttribute("mensagem","Usuário salvo com sucesso.");
 		
-		return "redirect:/usuarios/novo";
+		return new ModelAndView("redirect:/usuarios/novo");
 	}
 }
