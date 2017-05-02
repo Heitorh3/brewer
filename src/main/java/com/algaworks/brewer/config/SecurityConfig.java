dcb83ae8f199022145188ccea3c0a9f71e798a96
Heitor Neto <heitorh3@gmail.com>
2017-05-02 15:24:28 -0300

    Tela de login customizada

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,14 +15,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**")
+			.antMatchers("/images/**");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.csrf().disable();
 	}
-	
+		
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
