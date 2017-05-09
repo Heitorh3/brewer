cf12ed8df4fbf4bbbe6148c7473e6575d47e6f82
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2017-05-09 19:10:51 -0300

    Adicionando o atributo grupo na entidade usuarioFilter

--- src/main/java/com/algaworks/brewer/repository/filter/UsuarioFilter.java
+++ src/main/java/com/algaworks/brewer/repository/filter/UsuarioFilter.java
@@ -1,10 +1,16 @@
 package com.algaworks.brewer.repository.filter;
 
+import java.util.List;
+
+import com.algaworks.brewer.model.Grupo;
+
 public class UsuarioFilter {
 
 	private String nome;
 	private String email;
-
+	private List<Grupo> grupos;
+	
+	
 	public String getNome() {
 		return nome;
 	}
@@ -18,4 +24,11 @@ public class UsuarioFilter {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
+	}
+	
 }
