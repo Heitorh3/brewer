e2c8b029d3679a5ba7e242ec46a01cfc04e21974
Heitor Neto <heitorh3@gmail.com>
2017-05-12 09:00:26 -0300

    Adicionando o metodo getNomeCidadeSiglaEstado

--- src/main/java/com/algaworks/brewer/model/Edereco.java
+++ src/main/java/com/algaworks/brewer/model/Edereco.java
@@ -31,30 +31,35 @@ public class Edereco implements Serializable{
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
+	
 	public String getNumero() {
 		return numero;
 	}
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+	
 	public String getComplemento() {
 		return complemento;
 	}
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
+	
 	public String getCep() {
 		return cep;
 	}
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+	
 	public Cidade getCidade() {
 		return cidade;
 	}
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
+	
 	public Estado getEstado() {
 		return estado;
 	}
