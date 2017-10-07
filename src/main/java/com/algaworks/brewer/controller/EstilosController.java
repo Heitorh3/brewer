65c3cf35d9b7d416b46286a13675fb3ff00649ad
Heitor A Neto <heitorh3@gmail.com>
2017-10-07 11:23:21 -0300

    Implementando a edição de um estilo de cerveja.

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,12 +42,12 @@ public class EstilosController {
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Estilo estilo){
-		ModelAndView mv = new ModelAndView("estilo/CadastroEstilo");
-		return mv;
+		return new ModelAndView("estilo/CadastroEstilo");
 	}
 	
-	@RequestMapping(value = "/novo", method = RequestMethod.POST)
-	public ModelAndView cadastrar(@Valid Estilo estilo, BindingResult result, RedirectAttributes attributes, Model model){
+	//@RequestMapping(value = "/novo", method = RequestMethod.POST)
+	@PostMapping({"/novo", "{\\+d}"})
+	public ModelAndView salvar(@Valid Estilo estilo, BindingResult result, RedirectAttributes attributes, Model model){
 		if(result.hasErrors()){
 			model.addAttribute(estilo);
 			return novo(estilo);
@@ -102,7 +103,7 @@ public class EstilosController {
 	public ModelAndView editar(@PathVariable("codigo")Long codigo) {
 		Estilo estilo = estilos.findOne(codigo);
 		ModelAndView mv = novo(estilo);
-		mv.addObject(mv);
+		mv.addObject(estilo);
 		
 		return mv;
 	}
